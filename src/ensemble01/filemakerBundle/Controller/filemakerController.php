93ae2d9e34a6695ac7805ed11a58eee0185e6d97
manu7772 <manu7772@gmail.com>
2014-10-07 17:47:49 +0200

    ajout bundle filemaker

--- src/ensemble01/filemakerBundle/Controller/filemakerController.php
+++ src/ensemble01/filemakerBundle/Controller/filemakerController.php
@@ -4,10 +4,10 @@ namespace ensemble01\filemakerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class filemakerController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('ensemble01filemakerBundle:pages:index.html.twig', array('name' => $name));
+class filemakerController extends Controller {
+
+    public function indexAction($name) {
+        return $this->render('ensemble01filemakerBundle:pages:homepage.html.twig', array('name' => $name));
     }
+
 }
