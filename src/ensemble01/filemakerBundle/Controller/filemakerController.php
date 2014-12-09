a691ba89e564668cccbbe8f6f9b347f5aef69813
manu7772 <manu7772@gmail.com>
2014-12-09 12:21:49 +0100

    Divers

--- src/ensemble01/filemakerBundle/Controller/filemakerController.php
+++ src/ensemble01/filemakerBundle/Controller/filemakerController.php
@@ -18,16 +18,10 @@ class filemakerController extends fmController {
 		$data["User"] = $this->get('security.context')->getToken()->getUser();
 		$data["page"] = $page;
 		$_fm = $this->get('filemaker.database');
-		$_fm->log_user($data["User"]);
+		// $_fm->log_user($data["User"]);
 		switch ($page) {
 			case 'liste-rapports-complete':
-				$data['rapportsall'] = array(
-					1 => array("id" => 1, "ref" => "rapport_001"),
-					2 => array("id" => 2, "ref" => "rapport_002"),
-					3 => array("id" => 3, "ref" => "rapport_003"),
-					4 => array("id" => 4, "ref" => "rapport_004"),
-					5 => array("id" => 5, "ref" => "rapport_005"),
-					);
+				$data['locauxByLieux'] = $_fm->getRapports($dossier);
 				break;
 			case 'liste-lieux':
 				// liste des lieux
@@ -85,12 +79,71 @@ class filemakerController extends fmController {
 				// liste des bases de données FM
 				$data['databases'] = $_fm->getDatabases();
 				break;
+			case 'liste-servers':
+				// liste des bases de données FM
+				$data['servers'] = $_fm->getListOfServersNames();
+				break;
 			
 			default: // homepage, ou null
 				# code...
 				break;
 		}
 		return $this->render($this->verifVersionPage($data['page']), $data);
+	}
+
+
+	/**
+	 * Change de serveur courant
+	 * @param string $servernom - nom du serveur
+	 * @param string $page - page web
+	 * @return Response
+	 */
+	public function changeserverAction($servernom, $page = "homepage") {
+		$data = array();
+		$data["User"] = $this->get('security.context')->getToken()->getUser();
+		$data["page"] = $page;
+		$_fm = $this->get('filemaker.database')->setCurrentSERVER($servernom);
+		return $this->render($this->verifVersionPage($data['page']), $data);
+	}
+
+	/**
+	 * Change de base courante
+	 * @param string $basenom - nom du serveur
+	 * @param string $page - page web
+	 * @return Response
+	 */
+	public function changebaseAction($basenom, $page = "homepage") {
+		$data = array();
+		$data["User"] = $this->get('security.context')->getToken()->getUser();
+		$data["page"] = $page;
+		$_fm = $this->get('filemaker.database')->setCurrentBASE(null, $basenom);
+		return $this->render($this->verifVersionPage($data['page']), $data);
+	}
+
+
+	/**
+	 * Traite tous les rapports en BDD FM
+	 */
+	public function traitement_rapportsAction() {
+		$data = array();
+		$data["User"] = $this->get('security.context')->getToken()->getUser();
+		$data["page"] = $page;
+		$_fm = $this->get('filemaker.database');
+		$_fm->log_user($data["User"]);
+
+		$data['locauxByLieux'] = $_fm->getRapports(0);
+		$data['LieuxInRapport'] = $_fm->getRapportsLieux();
+
+		$data['result'] = array();
+		foreach ($data['locauxByLieux'] as $key => $rapport) {
+			// echo('Class : '.get_class($rapport)."<br />");
+			$id = $rapport->getField('id');
+			// $data['result'][$id] = $this->generate_rapports($rapport);
+			if(rand(0,1000) > 800) $data['result'][$id] = false;
+				else $data['result'][$id] = true;
+		}
+
+		return $this->render('ensemble01filemakerBundle:pages:result-rapports.html.twig', $data);
 	}
 
 	/**
@@ -101,6 +154,47 @@ class filemakerController extends fmController {
 		return $this->render('ensemble01filemakerBundle:menus:navbar.html.twig', $data);
 	}
 
+	/**
+	 * génère un rapport et l'enregistre sur disque
+	 * @param FileMaker_Record $rapport - objet rapport issu de FM
+	 * @return boolean (true si succès)
+	 */
+	protected function generate_rapports(FileMaker_Record $rapport) {
+		$data = array();
+		$data["id"] = $id;
+		$data["ref_rapport"] = $id."-1855-".$type;
+		$data["nom_logo_geodem"] = 'LogoGeodem.png';
+		$data["local"]["adresse"] = "11 rue des hérissons";
+		$data["local"]["cp"] = "27000";
+		$data["local"]["ville"] = "EVREUX";
+		$data["type_logement"] = "T4";
+		$data["annee_construction"] = "≤ 1998";
+		$data["commanditaire"]["nom"] = "SILOGE";
+		$data["commanditaire"]["adresse"] = "6bis Boulevard Chambaudoin";
+		$data["commanditaire"]["cp"] = "27009";
+		$data["commanditaire"]["ville"] = "EVREUX";
+		$data["commanditaire"]["telephone"] = "02 32 38 88 88";
+		$data["commanditaire"]["representant"]["civilite"] = "Monsieur";
+		$data["commanditaire"]["representant"]["nom"] = "DU TRANOY";
+		$data["commanditaire"]["representant"]["prenom"] = "";
+		$data["commanditaire"]["representant"]["fonction"] = "Ingénieur Travaux";
+		$data["societe"]["representant"]["civilite"] = "Monsieur";
+		$data["societe"]["representant"]["nom"] = "LEGENDRE";
+		$data["societe"]["representant"]["prenom"] = "Nicolas";
+		$data["societe"]["representant"]["fonction"] = "Directeur Opérationnel";
+
+		$filePDF = __DIR__.'/../../../../app/Resources/tools/html2pdf/html2pdf.class.php';
+		if(!file_exists($filePDF)) die('Service HTML2PDF non trouvé !');
+		require_once($filePDF);
+		$html2pdf = new \HTML2PDF('P', 'A4', 'fr', true, 'UTF-8', array(10, 8, 10, 10));
+		$html2pdf->pdf->SetDisplayMode('fullpage');
+		// $html = $this->renderView("ensemble01filemakerBundle:pdf:rapport_DAPP_001.html.twig", $data);
+		$html = $this->renderView("ensemble01filemakerBundle:pdf:rapport_".$type."_001.html.twig", $data);
+		$html2pdf->writeHTML($html, false);
+		$result = $html2pdf->Output($data["ref_rapport"].'.pdf');
+
+		return $result;
+	}
 
 	public function generate_rapportAction($id, $type, $mode, $format) {
 		$data = array();
