5444bd8751bb00196e09fadeccb18e3e45f0edf8
manu7772 <manu7772@gmail.com>
2015-03-10 14:18:16 +0100

    debugg graphs

--- src/ensemble01/filemakerBundle/Resources/public/startbootstrap-sb-admin-2-1.0.5/js/morris-data.js
+++ src/ensemble01/filemakerBundle/Resources/public/startbootstrap-sb-admin-2-1.0.5/js/morris-data.js
@@ -1,156 +1,161 @@
 $(function() {
 
-    if($('#morris-area-chart')) {
-        Morris.Area({
-            element: 'morris-area-chart',
-            data: [{
-                period: '2010 Q1',
-                iphone: 2666,
-                ipad: null,
-                itouch: 2647
-            }, {
-                period: '2010 Q2',
-                iphone: 2778,
-                ipad: 2294,
-                itouch: 2441
-            }, {
-                period: '2010 Q3',
-                iphone: 4912,
-                ipad: 1969,
-                itouch: 2501
-            }, {
-                period: '2010 Q4',
-                iphone: 3767,
-                ipad: 3597,
-                itouch: 5689
-            }, {
-                period: '2011 Q1',
-                iphone: 6810,
-                ipad: 1914,
-                itouch: 2293
-            }, {
-                period: '2011 Q2',
-                iphone: 5670,
-                ipad: 4293,
-                itouch: 1881
-            }, {
-                period: '2011 Q3',
-                iphone: 4820,
-                ipad: 3795,
-                itouch: 1588
-            }, {
-                period: '2011 Q4',
-                iphone: 15073,
-                ipad: 5967,
-                itouch: 5175
-            }, {
-                period: '2012 Q1',
-                iphone: 10687,
-                ipad: 4460,
-                itouch: 2028
-            }, {
-                period: '2012 Q2',
-                iphone: 8432,
-                ipad: 5713,
-                itouch: 1791
-            }],
-            xkey: 'period',
-            ykeys: ['iphone', 'ipad', 'itouch'],
-            labels: ['iPhone', 'iPad', 'iPod Touch'],
-            pointSize: 2,
-            hideHover: 'auto',
-            resize: true
-        });
-    }
+	$('.area-chart').each(function() {
+		// data = $.parseJSON($(this).attr('data-chart'));
+		data = [{
+				period: '2010 Q1',
+				iphone: 2666,
+				ipad: null,
+				itouch: 2647
+			}, {
+				period: '2010 Q2',
+				iphone: 2778,
+				ipad: 2294,
+				itouch: 2441
+			}, {
+				period: '2010 Q3',
+				iphone: 4912,
+				ipad: 1969,
+				itouch: 2501
+			}, {
+				period: '2010 Q4',
+				iphone: 3767,
+				ipad: 3597,
+				itouch: 5689
+			}, {
+				period: '2011 Q1',
+				iphone: 6810,
+				ipad: 1914,
+				itouch: 2293
+			}, {
+				period: '2011 Q2',
+				iphone: 5670,
+				ipad: 4293,
+				itouch: 1881
+			}, {
+				period: '2011 Q3',
+				iphone: 4820,
+				ipad: 3795,
+				itouch: 1588
+			}, {
+				period: '2011 Q4',
+				iphone: 15073,
+				ipad: 5967,
+				itouch: 5175
+			}, {
+				period: '2012 Q1',
+				iphone: 10687,
+				ipad: 4460,
+				itouch: 2028
+			}, {
+				period: '2012 Q2',
+				iphone: 8432,
+				ipad: 5713,
+				itouch: 1791
+			}];
+		Morris.Area({
+			element: $(this).attr('id'),
+			data: data,
+			xkey: 'period',
+			ykeys: ['iphone', 'ipad', 'itouch'],
+			labels: ['iPhone', 'iPad', 'iPod Touch'],
+			pointSize: 2,
+			hideHover: 'auto',
+			resize: true
+		});
+	});
 
-    $('.donut-donut').each(function() {
-        data = $.parseJSON($(this).attr('data-donut-chart'));
-        // alert('Donut : ' + $(this).attr('id'));
-        Morris.Donut({
-            element: $(this).attr('id'),
-            data: data,
-            resize: true
-        });
-    });
+	$('.donut-chart').each(function() {
+		data = $.parseJSON($(this).attr('data-chart'));
+		Morris.Donut({
+			element: $(this).attr('id'),
+			data: data,
+			resize: true
+		});
+	});
 
-    if($('#morris-bar-chart')) {
-        Morris.Bar({
-            element: 'morris-bar-chart',
-            data: [{
-                y: '2006',
-                a: 100,
-                b: 90
-            }, {
-                y: '2007',
-                a: 75,
-                b: 65
-            }, {
-                y: '2008',
-                a: 50,
-                b: 40
-            }, {
-                y: '2009',
-                a: 75,
-                b: 65
-            }, {
-                y: '2010',
-                a: 50,
-                b: 40
-            }, {
-                y: '2011',
-                a: 75,
-                b: 65
-            }, {
-                y: '2012',
-                a: 100,
-                b: 90
-            }],
-            xkey: 'y',
-            ykeys: ['a', 'b'],
-            labels: ['Series A', 'Series B'],
-            hideHover: 'auto',
-            resize: true
-        });
-    }
+	$('.bar-chart').each(function() {
+		// data = $.parseJSON($(this).attr('data-chart'));
+		data = [{
+				y: '2006',
+				a: 100,
+				b: 90
+			}, {
+				y: '2007',
+				a: 75,
+				b: 65
+			}, {
+				y: '2008',
+				a: 50,
+				b: 40
+			}, {
+				y: '2009',
+				a: 75,
+				b: 65
+			}, {
+				y: '2010',
+				a: 50,
+				b: 40
+			}, {
+				y: '2011',
+				a: 75,
+				b: 65
+			}, {
+				y: '2012',
+				a: 100,
+				b: 90
+			}];
+		Morris.Bar({
+			element: 'morris-bar-chart',
+			data: data,
+			xkey: 'y',
+			ykeys: ['a', 'b'],
+			labels: ['Series A', 'Series B'],
+			hideHover: 'auto',
+			resize: true
+		});
+	});
 
-    if($('#morris-line-chart')) {
-        Morris.Line({
-            element: 'morris-line-chart',
-            data: [{
-                y: '2006',
-                a: 100,
-                b: 90
-            }, {
-                y: '2007',
-                a: 75,
-                b: 65
-            }, {
-                y: '2008',
-                a: 50,
-                b: 40
-            }, {
-                y: '2009',
-                a: 75,
-                b: 65
-            }, {
-                y: '2010',
-                a: 50,
-                b: 40
-            }, {
-                y: '2011',
-                a: 75,
-                b: 65
-            }, {
-                y: '2012',
-                a: 100,
-                b: 90
-            }],
-            xkey: 'y',
-            ykeys: ['a', 'b'],
-            labels: ['Series A', 'Series B'],
-            hideHover: 'auto',
-            resize: true
-        });
-    }
+	$('.line-chart').each(function() {
+		// data = $.parseJSON($(this).attr('data-chart'));
+		data = [{
+				y: '2006',
+				a: 100,
+				b: 90
+			}, {
+				y: '2007',
+				a: 75,
+				b: 65
+			}, {
+				y: '2008',
+				a: 50,
+				b: 40
+			}, {
+				y: '2009',
+				a: 75,
+				b: 65
+			}, {
+				y: '2010',
+				a: 50,
+				b: 40
+			}, {
+				y: '2011',
+				a: 75,
+				b: 65
+			}, {
+				y: '2012',
+				a: 100,
+				b: 90
+			}];
+		Morris.Line({
+			element: $(this).attr('id'),
+			data: data,
+			xkey: 'y',
+			ykeys: ['a', 'b'],
+			labels: ['Series A', 'Series B'],
+			hideHover: 'auto',
+			resize: true
+		});
+	});
 
 });
